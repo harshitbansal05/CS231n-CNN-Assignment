0162bd5a30a57ea6d786448aee5e14e98cd829ef
iamharsshit <harshit.bansalec@gmail.com>
2017-12-25 13:58:19 +0530

    assignment 1 neural-net completed

--- assignment1/cs231n/classifiers/neural_net.py
+++ assignment1/cs231n/classifiers/neural_net.py
@@ -36,7 +36,7 @@ class TwoLayerNet(object):
     - output_size: The number of classes C.
     """
     self.params = {}
-    self.params['W1'] = std * np.random.randn(input_size, hidden_size)
+    self.params['W1'] = std * np.random.randn(input_size, hidden_size)  
     self.params['b1'] = np.zeros(hidden_size)
     self.params['W2'] = std * np.random.randn(hidden_size, output_size)
     self.params['b2'] = np.zeros(output_size)
@@ -71,12 +71,18 @@ class TwoLayerNet(object):
 
     # Compute the forward pass
     scores = None
+    num_train = X.shape[0]
     #############################################################################
     # TODO: Perform the forward pass, computing the class scores for the input. #
     # Store the result in the scores variable, which should be an array of      #
     # shape (N, C).                                                             #
     #############################################################################
-    pass
+    # We have to apply ReLU non-linearlity
+    hidden_zero_matrix = np.zeros((X.shape[0], W1.shape[1]))
+    output_zero_matrix = np.zeros((X.shape[0], W2.shape[1]))
+    f = lambda x: 1.0/(1.0 + np.exp(-x))
+    scores = np.matmul(X, W1) + b1
+    scores = np.matmul(np.maximum(scores, hidden_zero_matrix), W2) + b2
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -86,14 +92,23 @@ class TwoLayerNet(object):
       return scores
 
     # Compute the loss
-    loss = None
+    loss = 0.0
     #############################################################################
     # TODO: Finish the forward pass, and compute the loss. This should include  #
     # both the data loss and L2 regularization for W1 and W2. Store the result  #
     # in the variable loss, which should be a scalar. Use the Softmax           #
     # classifier loss.                                                          #
     #############################################################################
-    pass
+    scores -= np.max(scores)
+    correct_class_scores = np.exp(np.choose(y, scores.transpose()))
+    exp_scores = np.sum(np.exp(scores), axis = 1) 
+    loss = -np.sum(np.log(np.divide(correct_class_scores, exp_scores)))
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= num_train
+    # Add regularization to the loss.
+    loss += reg * np.sum(np.multiply(W1, W1))
+    loss += reg * np.sum(np.multiply(W2, W2))
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -105,7 +120,41 @@ class TwoLayerNet(object):
     # and biases. Store the results in the grads dictionary. For example,       #
     # grads['W1'] should store the gradient on W1, and be a matrix of same size #
     #############################################################################
-    pass
+    dW2 = np.zeros_like(W2)
+    softmax_matrix = np.exp(scores) / exp_scores[:,None]
+
+    row_indices = W2.shape[1] * np.arange(num_train)
+    add_factor = np.add(row_indices, y)
+    replace_factor = np.ones(num_train)
+    sub_matrix = np.zeros(softmax_matrix.shape)
+    np.put(sub_matrix, add_factor, replace_factor)
+
+    hidden_X = np.maximum(np.matmul(X, W1) + b1, hidden_zero_matrix)
+    dW2 = np.matmul(hidden_X.transpose(), softmax_matrix - sub_matrix)
+    dW2 /= num_train
+    dW2[:,:] += 2 * reg * W2[:,:]
+    
+    # Evalate the matrix db2
+    bias_X = np.ones(num_train)
+    db2 = np.matmul(bias_X.transpose(), softmax_matrix - sub_matrix)
+    db2 /= num_train
+
+    # Evalate the matrix dW1
+    back_grad_W1 = np.matmul(softmax_matrix, W2.transpose()) - W2[:,y].transpose()
+    hidden_X[hidden_X != 0] = 1
+    dW1 = np.matmul(X.transpose(), np.multiply(hidden_X, back_grad_W1))
+    dW1 /= num_train
+    dW1[:,:] += 2 * reg * W1[:,:]
+
+    # Evalate the matrix db1
+    db1 = np.matmul(bias_X.transpose(), np.multiply(hidden_X, back_grad_W1))
+    db1 /= num_train
+
+    # Assignment to the grads dictionary
+    grads['W1'] = dW1
+    grads['b1'] = db1
+    grads['W2'] = dW2
+    grads['b2'] = db2
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
@@ -149,7 +198,9 @@ class TwoLayerNet(object):
       # TODO: Create a random minibatch of training data and labels, storing  #
       # them in X_batch and y_batch respectively.                             #
       #########################################################################
-      pass
+      random_indices = np.random.choice(num_train, batch_size, replace = False)
+      X_batch = X[random_indices]
+      y_batch = y[random_indices]
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
@@ -164,7 +215,10 @@ class TwoLayerNet(object):
       # using stochastic gradient descent. You'll need to use the gradients   #
       # stored in the grads dictionary defined above.                         #
       #########################################################################
-      pass
+      self.params['W1'] -= learning_rate * grads['W1']
+      self.params['b1'] -= learning_rate * grads['b1']
+      self.params['W2'] -= learning_rate * grads['W2']
+      self.params['b2'] -= learning_rate * grads['b2']
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
@@ -209,7 +263,13 @@ class TwoLayerNet(object):
     ###########################################################################
     # TODO: Implement this function; it should be VERY simple!                #
     ###########################################################################
-    pass
+    # Unpack variables from the params dictionary
+    W1, b1 = self.params['W1'], self.params['b1']
+    W2, b2 = self.params['W2'], self.params['b2']
+    N, D = X.shape
+    hidden_zero_matrix = np.zeros((X.shape[0], W1.shape[1]))
+    scores = np.matmul(np.maximum(np.matmul(X, W1) + b1, hidden_zero_matrix), W2) + b2 
+    y_pred = np.argmax(scores, axis = 1) 
     ###########################################################################
     #                              END OF YOUR CODE                           #
     ###########################################################################
